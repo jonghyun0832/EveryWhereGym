99b80d7b706a928f169452b6d298f49698bdd7d6
J.Hyun <jonghyun9606@gmail.com>
2022-01-11 20:15:20 +0900

    ADD SignUp

---
+++ app/src/main/java/com/example/everywheregym/SingupInfo.java
@@ -0,0 +1,13 @@
+package com.example.everywheregym;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SingupInfo {
+
+    @Expose
+    @SerializedName("email") private String email;
+
+
+
+}
---
+++ app/src/main/java/com/example/everywheregym/UserInfo.java
@@ -0,0 +1,81 @@
+package com.example.everywheregym;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UserInfo {
+
+    @Expose
+    @SerializedName("email") private String email;
+
+    @Expose
+    @SerializedName("cnum") private String cnum;
+
+    @Expose
+    @SerializedName("nickname") private boolean nameDuplicate;
+
+    @Expose
+    @SerializedName("success") private boolean success;
+
+
+
+//    @Expose
+//    @SerializedName("password") private String password;
+//
+//    @Expose
+//    @SerializedName("nickname") private String nickname;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCnum() {
+        return cnum;
+    }
+
+    public void setCnum(String cnum) {
+        this.cnum = cnum;
+    }
+
+    public boolean isNameDuplicate() {
+        return nameDuplicate;
+    }
+
+    public void setNameDuplicate(boolean nameDuplicate) {
+        this.nameDuplicate = nameDuplicate;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    //    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getNickname() {
+//        return nickname;
+//    }
+//
+//    public void setNickname(String nickname) {
+//        this.nickname = nickname;
+//    }
+
+    //    @Expose
+//    @SerializedName("") private String email;
+
+
+}
